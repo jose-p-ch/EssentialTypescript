752ae82031db048cd17200f408b08d7ec516138d
Jose <jose.p.ch@gmail.com>
2020-07-01 09:02:35 -0400

    Chapter 1: todo list example

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,2 +1,16 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
+]
+
+let collection = new TodoCollection("Jose", todos);
+
 console.clear();
-console.log("Jose's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
