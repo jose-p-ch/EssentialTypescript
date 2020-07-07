2684b1fa16c138cfd617ccd49e0ab2e95083d345
Jose <jose.p.ch@gmail.com>
2020-07-07 08:15:37 -0400

    Chapter 3: started primer example

---
+++ primer/index.js
@@ -0,0 +1,18 @@
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+let bootPrice = "100";
+console.log(`Boot price: ${bootPrice}`);
+
+if (hatPrice == bootPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = hatPrice + bootPrice;
+console.log(`Total price: ${totalPrice}`);
+
+let myVariable = "Adam";
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
