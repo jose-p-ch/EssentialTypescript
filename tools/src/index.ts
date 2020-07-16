128bffba16fb8ab83a49f579ff00ca5276f1cf4e
Jose <jose.p.ch@gmail.com>
2020-07-16 09:23:23 -0400

    Chapter 6: debugging initial changes

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,4 +1,11 @@
+import { sum } from "./calc";
+
 let printMessage = (msg: String): void => console.log(`Message: ${ msg }`);
 
 let message = ("Hello, TypeScript");
 printMessage(message);
+
+debugger;
+
+let total = sum(100, 200, 300);
+console.log(`Total: ${total}`)
