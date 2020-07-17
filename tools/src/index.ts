5053d7205392d4de102b3af17f5d94deed752f97
Jose <jose.p.ch@gmail.com>
2020-07-17 10:04:50 -0400

    Chapter 6: more debug. linting/prep for jest.

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,11 +1,12 @@
 import { sum } from "./calc";
 
-let printMessage = (msg: String): void => console.log(`Message: ${ msg }`);
+let printMessage = (msg: string): void => console.log(`Message: ${ msg }`);
 
 let message = ("Hello, TypeScript");
 printMessage(message);
 
+// tslint:disable-next-line no-debugger
 debugger;
 
 let total = sum(100, 200, 300);
-console.log(`Total: ${total}`)
+console.log(`Total: ${total}`);
