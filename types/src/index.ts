af35f0a0fa0f06e8653fb09cadf2eac291d12cc4
Jose <jose.p.ch@gmail.com>
2020-07-20 09:03:55 -0400

    Chapter 7: using type assertions

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,14 +1,10 @@
-function calculateTax(amount): any {
-    return `$${(amount * 1.2).toFixed(2)}`;
+function calculateTax(amount: number, format: boolean): string | number {
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
 
-let price = 100;
-let taxAmount = calculateTax(price);
-let halfShare = taxAmount / 2;
+let taxNumber = calculateTax(100, false) as number;
+let taxString = calculateTax(100, true) as string;
 
-let personVal = calculateTax("Bob");
-
-console.log(`Price: ${price}`)
-console.log(`Full amount in tax: ${taxAmount}`);
-console.log(`Half share: ${halfShare}`);
-console.log(`Name: ${personVal}`);
+console.log(`Number Value: ${taxNumber.toFixed(2)}`);
+console.log(`String Value: ${taxString.charAt(0)}`);
