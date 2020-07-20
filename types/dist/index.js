3dbf4f5127a20eb211f90d3009884ca1bdf82cdd
Jose <jose.p.ch@gmail.com>
2020-07-20 08:42:46 -0400

    Chapter 7: type annotation

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,8 +1,8 @@
-let myVar;
-console.log(`${myVar} = ${typeof myVar}`);
-myVar = 12;
-console.log(`${myVar} = ${typeof myVar}`);
-myVar = "Hello";
-console.log(`${myVar} = ${typeof myVar}`);
-myVar = true;
-console.log(`${myVar} = ${typeof myVar}`);
+function calculateTax(amount) {
+    return amount * 1.2;
+}
+let price = 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,8 +1,10 @@
-let myVar;
-console.log(`${myVar} = ${typeof myVar}`)
-myVar = 12;
-console.log(`${myVar} = ${typeof myVar}`)
-myVar = "Hello";
-console.log(`${myVar} = ${typeof myVar}`)
-myVar = true;
-console.log(`${myVar} = ${typeof myVar}`)
+function calculateTax(amount: number): number {
+    return amount * 1.2;
+}
+
+let price: number = 100;
+let taxAmount: number = calculateTax(price);
+let halfShare: number = taxAmount / 2;
+
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
