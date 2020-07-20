a1e181214a780502b397ea5935f44f52d7dbcbf5
Jose <jose.p.ch@gmail.com>
2020-07-20 08:46:27 -0400

    Chapter 7: implicit static types

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,10 +1,11 @@
-function calculateTax(amount: number): number {
-    return amount * 1.2;
+function calculateTax(amount: number) {
+    return (amount * 1.2).toFixed(2);
 }
 
-let price: number = 100;
-let taxAmount: number = calculateTax(price);
-let halfShare: number = taxAmount / 2;
+let price = 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
 
+console.log(`Price: ${price}`)
 console.log(`Full amount in tax: ${taxAmount}`);
 console.log(`Half share: ${halfShare}`);
