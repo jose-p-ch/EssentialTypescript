b1350df3cb8f378af93f974f17dc8952c5fd3139
Jose <jose.p.ch@gmail.com>
2020-07-23 08:56:23 -0400

    Chapter 8: strictNullChecks

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,6 +1,7 @@
 function calculateTax(amount, discount = 0, ...extraFees) {
-    return (amount * 1.2) - discount
-        + extraFees.reduce((total, val) => total + val, 0);
+    if (amount != null) {
+        return (amount * 1.2 - discount + extraFees.reduce((total, val) => total + val, 0));
+    }
 }
 let taxValue = calculateTax(100, 0);
 console.log(`discount 0: ${taxValue}`);
@@ -10,3 +11,5 @@ taxValue = calculateTax(100, 10, 20);
 console.log(`discount 10, extra 10: ${taxValue}`);
 taxValue = calculateTax(100, 10, 20, 1, 30, 7);
 console.log(`discount 10, extra 10,20,1,30,7: ${taxValue}`);
+taxValue = calculateTax(null, 0);
+console.log(`null input: ${taxValue}`);
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,16 +1,26 @@
-function calculateTax(amount: number, discount: number = 0, ...extraFees: number[]) {
-  return (amount * 1.2) - discount
-  + extraFees.reduce((total, val) => total + val, 0);
+function calculateTax(
+  amount: number | null,
+  discount: number = 0,
+  ...extraFees: number[]
+) {
+  if (amount != null) {
+    return (
+      amount * 1.2 - discount + extraFees.reduce((total, val) => total + val, 0)
+    );
+  }
 }
 
 let taxValue = calculateTax(100, 0);
 console.log(`discount 0: ${taxValue}`);
 
-taxValue = calculateTax(100)
+taxValue = calculateTax(100);
 console.log(`discount undefined: ${taxValue}`);
 
-taxValue = calculateTax(100, 10, 20)
+taxValue = calculateTax(100, 10, 20);
 console.log(`discount 10, extra 10: ${taxValue}`);
 
-taxValue = calculateTax(100, 10, 20, 1, 30, 7)
+taxValue = calculateTax(100, 10, 20, 1, 30, 7);
 console.log(`discount 10, extra 10,20,1,30,7: ${taxValue}`);
+
+taxValue = calculateTax(null, 0);
+console.log(`null input: ${taxValue}`);
