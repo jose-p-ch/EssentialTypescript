92d3b7219463824920816a24c436472e2eb459e8
Jose <jose.p.ch@gmail.com>
2020-08-03 13:46:52 -0400

    Chapter 10: methods & strictNullChecks

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,7 +1,22 @@
-let hat = { name: "Hat", price: 100};
-let gloves = { name: "Gloves", price: 75};
-let umbrella = {name : "Umbrella", price: 30, waterproof: true};
+enum Feature {
+  Waterproof,
+  Insulated,
+}
 
-let products: {name: string, price?: number, waterproof?: boolean}[] = [hat, gloves, umbrella];
+let hat = { name: "Hat", price: 100 };
+let gloves = { name: "Gloves", price: 75 };
+let umbrella = {
+  name: "Umbrella",
+  price: 30,
+  hasFeature: (feature) => feature === Feature.Waterproof,
+};
 
-products.forEach(prod => console.log(`${prod.name}: ${prod.price} Waterproof: ${prod.waterproof}`));
+let products: { name: string; price?: number; hasFeature?(Feature): boolean }[] = [
+  hat,
+  gloves,
+  umbrella,
+];
+
+products.forEach((prod) =>
+  console.log(`${prod.name}: ${prod.price} Waterproof: ${prod.hasFeature ? prod.hasFeature(Feature.Waterproof) : "false"}`)
+);
