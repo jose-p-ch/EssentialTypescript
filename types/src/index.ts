de54962853cfad98620a524921f4fb1949ed6b11
Jose <jose.p.ch@gmail.com>
2020-08-04 10:53:44 -0400

    Chapter 10: type guard with predicate function

--- types/src/index.ts
+++ types/src/index.ts
@@ -17,8 +17,12 @@ let bob = { id: "bsmith", name: "Bob", city: "London"};
 
 let dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob];
 
+function isPerson(testObj: any): testObj is Person {
+    return testObj.city !== undefined;
+}
+
 dataItems.forEach(item => {
-    if ("city" in item){
+    if (isPerson(item)){
         console.log(`Person: ${item.name}, City: ${item.city}`);
     } else {
         console.log(`Product: ${item.name}, Price: ${item.price}`);
