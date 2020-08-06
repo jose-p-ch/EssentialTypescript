2bd8b947b6ea5d26e875cf797d9530f15f191714
Jose <jose.p.ch@gmail.com>
2020-08-06 08:27:27 -0400

    Chapter 10: merging methods

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,17 +1,15 @@
-let typeTest = {}.contact;
-let person1 = {
+let person = {
     id: "bsmith",
     name: "Bob Smith",
     city: "London",
     company: "Acme Co",
     dept: "Sales",
-    contact: { name: "Alice", phone: 65123456543 },
+    getContact(field) {
+        return typeof field === "string" ? "Alice" : 65123456543;
+    }
 };
-let person2 = {
-    id: "dpeters",
-    name: "Dora Peters",
-    city: "New York",
-    company: "Acme Co",
-    dept: "Development",
-    contact: { name: "Alice", phone: 65123456543 },
-};
+let typeTest = person.getContact;
+let stringParamTypeTest = person.getContact("Alice");
+let numberParamTypeTest = person.getContact(123);
+console.log(`Contact: ${person.getContact("Alice")}`);
+console.log(`Contact: ${person.getContact(123)}`);
