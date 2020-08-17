5804de4df1cee21f050bebfb8b682f1db3aac89e
Jose <jose.p.ch@gmail.com>
2020-08-17 09:57:27 -0400

    Ch12: multiple type parameters

--- types/dist/index.js
+++ types/dist/index.js
@@ -12,23 +12,17 @@ class DataCollection {
         this.items = [];
         this.items.push(...initialItems);
     }
-    add(newItem) {
-        this.items.push(newItem);
-    }
-    getNames() {
-        return this.items.map(item => item.name);
-    }
-    getItem(index) {
-        return this.items[index];
+    collate(targetData, itemProp, targetProp) {
+        let results = [];
+        this.items.forEach(item => {
+            let match = targetData.find(d => d[targetProp] === item[itemProp]);
+            if (match !== undefined) {
+                results.push({ ...match, ...item });
+            }
+        });
+        return results;
     }
 }
 let peopleData = new DataCollection(people);
-let firstPerson = peopleData.getItem(0);
-console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-console.log(`Person Names: ${peopleData.getNames().join(", ")}`);
-let productData = new DataCollection(products);
-let firstProduct = productData.getItem(0);
-console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
-console.log(`Product Names: ${productData.getNames().join(", ")}`);
-let cityData = new DataCollection(cities);
-console.log(`City Names: ${cityData.getNames().join(", ")}`);
+let collatedData = peopleData.collate(cities, "city", "name");
+collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
