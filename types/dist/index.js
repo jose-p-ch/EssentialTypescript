d6a143200a72d3a19d66a1f1ee734518512b1c13
Jose <jose.p.ch@gmail.com>
2020-08-17 09:20:31 -0400

    Ch12: generic class

--- types/dist/index.js
+++ types/dist/index.js
@@ -13,16 +13,16 @@ class DataCollection {
     add(newItem) {
         this.items.push(newItem);
     }
-    getNames() {
-        return this.items.map(item => item.name);
-    }
+    /* getNames(): string[] {
+      return this.items.map(item => item.name);
+    } */
     getItem(index) {
         return this.items[index];
     }
 }
 let peopleData = new DataCollection(people);
-console.log(`Names: ${peopleData.getNames().join(", ")}`);
+//console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
-if (firstPerson instanceof dataTypes_1.Person) {
-    console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-}
+//if(firstPerson instanceof Person){
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+//}
--- types/src/index.ts
+++ types/src/index.ts
@@ -7,30 +7,30 @@ new Product("Hat", 25)];
 
 type dataType = Person | Product;
 
-class DataCollection {
-  private items: dataType[] = [];
+class DataCollection<T> {
+  private items: T[] = [];
 
-  constructor(initialItems: dataType[]){
+  constructor(initialItems: T[]){
     this.items.push(...initialItems);
   }
 
-  add(newItem: dataType){
+  add(newItem: T){
     this.items.push(newItem);
   }
 
-  getNames(): string[] {
+  /* getNames(): string[] {
     return this.items.map(item => item.name);
-  }
+  } */
 
-  getItem(index: number): dataType {
+  getItem(index: number): T {
     return this.items[index];
   }
 }
 
-let peopleData = new DataCollection(people);
+let peopleData = new DataCollection<Person>(people);
 
-console.log(`Names: ${peopleData.getNames().join(", ")}`);
+//console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
-if(firstPerson instanceof Person){
+//if(firstPerson instanceof Person){
   console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-}
+//}
