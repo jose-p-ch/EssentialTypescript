b5051a1a55a80176abc1e8ee4a82782301a66edc
Jose <jose.p.ch@gmail.com>
2020-08-22 12:45:22 -0400

    Ch13: generic & mapped type

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,32 +1,11 @@
 import { City, Person, Employee, Product } from "./dataTypes";
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-
-//type shapeType = { name: string };
-
-class Collection<T, K extends keyof T> implements Iterable<T>{
-  private items: Map<T[K], T>;
-
-  constructor(initialItems: T[] = [], private propertyName: K) {
-    this.items = new Map<T[K], T>();
-    this.add(...initialItems);
-  }
-  add(...newItems: T[]): void {
-    newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
-  }
-  get(key: T[K]): T {
-    return this.items.get(key);
-  }
-  get count(): number {
-    return this.items.size;
-  }
-
-  [Symbol.iterator](): Iterator<T> {
-    return this.items.values();
-  }
+ 
+type Mapped<T> = {
+  [P in keyof T] : T[P];
 }
 
-let productCollection: Collection<Product, "name"> = new Collection(products, "name");
-console.log(`There are ${ productCollection.count } products`);
+let p: Mapped<Product> = { name: "Kayak", price: 275};
+console.log(`Mapped type: ${p.name}, ${p.price}`);
 
-let itemByKey = productCollection.get("Hat");
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
+let c: Mapped<City> = { name: "London", population: 8136000};
+console.log(`Mapped type: ${c.name}, ${c.population}`);
