d021243ae8d505f73526728bf4180d33da2854e5
Jose <jose.p.ch@gmail.com>
2020-08-22 11:35:24 -0400

    Ch13: index type restricting generic

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,25 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-class Collection {
-    constructor(initialItems = []) {
-        this.items = new Map();
-        this.add(...initialItems);
-    }
-    add(...newItems) {
-        newItems.forEach(newItem => this.items.set(newItem.name, newItem));
-    }
-    get(name) {
-        return this.items.get(name);
-    }
-    get count() {
-        return this.items.size;
-    }
-    [Symbol.iterator]() {
-        return this.items.values();
-    }
+function getValue(item, keyname) {
+    console.log(`Value: ${item[keyname]}`);
 }
-let productCollection = new Collection(products);
-console.log(`There are ${productCollection.count} products`);
-[...productCollection].forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
+let p = new dataTypes_1.Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+let e = new dataTypes_1.Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
