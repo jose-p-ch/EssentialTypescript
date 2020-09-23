091c8a662b2f3a8e2e16016426d14588f9a800b4
Jose <jose.p.ch@gmail.com>
2020-09-23 09:37:28 -0400

    Ch15: css & DOM display

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -1,6 +1,29 @@
 import { LocalDataSource } from "./data/localDataSource";
+import { DomDisplay } from "./domDisplay";
+import "bootstrap/dist/css/bootstrap.css";
 
-async  function displayData(): Promise<string> {
+let ds = new LocalDataSource();
+
+async function displayData(): Promise<HTMLElement> {
+    let display = new DomDisplay();
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+    return display.getContent();
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        displayData().then(elem => {
+            let rootElement = document.getElementById("app");
+            rootElement.innerHTML = "";
+            rootElement.appendChild(elem);
+        });
+    }
+};
+
+/* async function displayData(): Promise<string> {
     let ds = new LocalDataSource();
     let allProducts = await ds.getProducts("name");
     let categories = await ds.getCategories();
@@ -15,4 +38,4 @@ async  function displayData(): Promise<string> {
     return result;
 }
 
-displayData().then(res => console.log(res));
+displayData().then(res => console.log(res)); */
